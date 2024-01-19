e3a111101e8ee19d7787a74f5160bd4ae45892af
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2024-01-19 16:31:12 +0000

    loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -301,9 +301,17 @@ for(let rep = 1; rep <= 10; rep++) {
 }*/
 
 let rep = 1;
-while(rep <= 10) {
+while (rep <= 10) {
     console.log(`WHILE: Lifting weights repetition ${rep}`);
     rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
 }
 
 // ASSIGNMENTS

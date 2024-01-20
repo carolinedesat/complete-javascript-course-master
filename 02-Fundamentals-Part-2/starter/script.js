f1a55078fe389279033132bf2bd36cd9a4999084
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2024-01-20 13:13:46 +0000

    section3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -298,7 +298,7 @@ for(let exercise = 1; exercise <= 3; exercise++) {
 
 for(let rep = 1; rep <= 10; rep++) {
     console.log(`FOR: Lifting weights repetition ${rep}`);
-}*/
+}
 
 let rep = 1;
 while (rep <= 10) {
@@ -312,7 +312,7 @@ while (dice !== 6) {
     console.log(`You rolled a ${dice}`);
     dice = Math.trunc(Math.random() * 6) + 1;
     if (dice === 6) console.log('Loop is about to end...');
-}
+}*/
 
 // ASSIGNMENTS
 
@@ -432,3 +432,30 @@ if (mark.BMI > john.BMI) {
 } else if (john.BMI > mark.BMI) {
     console.log(`${john.fullName}'s BMI (${john.BMI}) is higher than ${mark.fullName}'s BMI (${mark.BMI})`);
 }*/
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const tips = [];
+  const totals = [];
+  
+  for(let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+  }
+  
+  console.log(bills, tips, totals);
+    
+  function calcAverage(arr) {
+      let sum = 0;
+      for(let i = 0; i < arr.length; i++) {
+          //sum = sum + arr[i];
+          sum += arr[i];
+      }
+      return sum / arr.length;
+  }
+  
+  console.log(`The average is: ${calcAverage(totals)}`);

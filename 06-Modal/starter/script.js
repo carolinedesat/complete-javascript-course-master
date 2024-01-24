260cad2cc124bcccd05ba2685010140239fd5eae
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2024-01-24 11:57:55 +0000

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,8 +3,20 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModal.length; i++)
-    console.log(btnsShowModal[i].textContent);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);

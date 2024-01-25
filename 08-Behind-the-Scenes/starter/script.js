7aa5afadf5b2a1882a248f6033f1ca24f359a776
Caroline de Sá Teixeira <caroline.desat@gmail.com>
2024-01-25 20:20:42 +0000

    scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are the ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1991 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating new variable with same name as outer scope's variable
+      const firstName = 'Conn';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Caroline';
+calcAge(1994);
